5b2e2d5aa71974ae41852b3ce8d169116013a184
NGOGIAHUY24 <huyngse183274@fpt.edu.vn>
2025-09-25 23:16:27 +0700

    swip: do profile in user

--- src/app/user/profile/page.tsx
+++ src/app/user/profile/page.tsx
@@ -1,6 +1,9 @@
+"use client"
 import { ButtonStyled } from "@/components"
+import SexPicker from "@/components/modules/UserItem/SexPicker"
 import AvaterStyled from "@/components/styled/AvatarStyled"
 import DatePickerStyled from "@/components/styled/DatePickerStyled"
+import { Input } from "@heroui/react"
 import { NotePencilIcon } from "@phosphor-icons/react/dist/ssr"
 
 import React from "react"
@@ -9,36 +12,41 @@ export default function ProfilePage() {
     const img = "https://i.pravatar.cc/150?u=a042581f4e29026024d"
     // const { t } = useTranslation()
     return (
-        <div>
-            <div className="text-3xl mb-4 font-bold">Thông tin tài khoản </div>
-            <div className="flex justify-between">
-                <div>
+        <div className="w-[984]">
+            <div className="text-3xl mb-4 p-4 font-bold">Thông tin tài khoản </div>
+
+            <div className="flex gap-60">
+                <div className="ml-6">
                     <AvaterStyled name="Ngo Gia Huy" img={img} />
                 </div>
-                <div className="flex gap-3">
-                    <ButtonStyled className="bg-white border border-primary text-primary hover:bg-primary hover:text-black ">
-                        <div>
-                            <NotePencilIcon />
-                        </div>
-                        Chỉnh sửa thông tin
-                    </ButtonStyled>
 
-                    <ButtonStyled className="bg-white border border-primary text-red-600 hover:bg-red-600 hover:text-black ">
-                        <div>
-                            <NotePencilIcon />
-                        </div>
-                        Xóa tài khoản
-                    </ButtonStyled>
-                </div>
+                <ButtonStyled
+                    className="bg-white border border-primary text-primary
+                                 hover:bg-primary hover:text-black mt-9"
+                >
+                    <div>
+                        <NotePencilIcon />
+                    </div>
+                    Chỉnh sửa thông tin
+                </ButtonStyled>
             </div>
-            <div>
-                <DatePickerStyled />
+            <div className="flex justify-around mt-10">
+                <DatePickerStyled className="w-75" />
+                <SexPicker label="Sex" placeholder="Chọn giới tính" className="w-75" />
             </div>
+
             <div>
-                <DatePickerStyled />
-            </div>
-            <div>
-                <DatePickerStyled />
+                {/* Phone */}
+
+                {/* Email */}
+                <Input
+                    isReadOnly
+                    className="w-75"
+                    defaultValue="junior@heroui.com"
+                    label="Email"
+                    type="email"
+                    variant="bordered"
+                />
             </div>
         </div>
     )
